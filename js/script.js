7b069e4fe916bdfb1a79dc164ebd2fdf07d1559e
Glitch (emmanuelio-giphy) <none>
2022-04-13 22:26:19 +0000

    🏦🚲 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
-let apilink = 
-    "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=rQrXp8Skecms5IYqoh0BS4HeQW9L35LY";
+$(".search-button").click(function () {
+  let num = Math.floor(Math.random() * 50);
+  let userInput = $("input").val();
+  let apilink = `https://api.giphy.com/v1/gifs/search?q=${userInput}&rating=pg&api_key=rQrXp8Skecms5IYqoh0BS4HeQW9L35LY`;
 
-
-fetch(apilink)
-  .then(function(response) {
-  return response.json();
-  })
- .then(function(data) {
-		console.log(data.data[0].images.original.url);
-  })
- .then(function(data){
-    $(".main").append(`<img src="${data.data.images.original.url}">`);
-  })
-
+  fetch(apilink)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data.data[num].images.original.url);
+      $(".display").html(`<img src="${data.data[num].images.original.url}">`);
+    });
+});
